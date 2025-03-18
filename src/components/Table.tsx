fa665c584b47df77a9f1690b05347ea1c668e1d9
Muhammad Zacky Abdillah <muhammadzackya86@gmail.com>
2025-03-18 20:59:59 +0700

    Study Case Generic TS

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,29 +1,32 @@
 import React from "react";
 
-export type ColumnDefs = {
+export type ColumnDefs<T> = {
     title: string;
 } & (
         | {
-            field: string;
+            field: keyof T;
         }
         | {
-            render: (rowData: Record<string, any>) => React.ReactNode;
+            render: (rowData: T) => React.ReactNode;
         }
     );
 
-const Table = ({
-    columnDefs,
-    data,
-}: {
-    columnDefs: ColumnDefs[];
-    data: Record<string, any>[];
-}) => {
+type TableProps<T> = {
+    columnDefs: ColumnDefs<T>[];
+    data: T[];
+};
+
+const Table = <T,>({ columnDefs, data }: TableProps<T>) => {
     return (
         <table className="min-w-full divide-y overflow-hidden rounded-lg divide-gray-800 shadow-lg">
             <thead className="bg-green-200">
                 <tr>
                     {columnDefs.map((column, index) => (
-                        <th key={index} scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        <th
+                            key={index}
+                            scope="col"
+                            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                        >
                             {column.title}
                         </th>
                     ))}
@@ -34,7 +37,11 @@ const Table = ({
                     <tr key={index} className="cursor-pointer hover:bg-gray-100">
                         {columnDefs.map((column, index) => (
                             <td key={index} className="px-6 py-4 whitespace-nowrap">
-                                {"field" in column ? rowData[column.field!] : column.render ? column.render(rowData) : null}
+                                {"field" in column
+                                    ? String(rowData[column.field as keyof T])
+                                    : "render" in column && column.render
+                                    ? column.render(rowData)
+                                    : null}
                             </td>
                         ))}
                     </tr>
